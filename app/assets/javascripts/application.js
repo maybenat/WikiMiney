ac239ad938c1a8c038cb812b4fae5dac3b113683
Sam Olds <samolds@gmail.com>
2015-04-27 23:55:07 -0600

    get highcharts working and the top page designed nicely ish

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,13 +9,11 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//
-//
+
 //= require jquery
-//= require jquery_ujs
+//= require jquery_ujs   
 //= require twitter/bootstrap
 //= require turbolinks
-//= require_tree .
-
 //= require highcharts
 //= require highcharts/modules/drilldown
+//= require_tree .
