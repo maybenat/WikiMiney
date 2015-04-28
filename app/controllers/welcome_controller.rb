1741a01d8d000a9eae91700172991a297ea13225
Sam Olds <samolds@gmail.com>
2015-04-28 11:55:44 -0600

    think all of the pages are done being laid out. highcharts js time!

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,19 @@
 class WelcomeController < ApplicationController
+  $months = []
+  $months.unshift(['October', '10'])
+  $months.unshift(['November', '11'])
+  $months.unshift(['December', '12'])
+
+  $days = []
+  for i in 1..31
+    $days.unshift(['%d' i, '%d' i])
+  end
+  $days.unshift(['Entire Month Aggregate', 'all'])
+
+  $years = []
+  $years.unshift(['2008', '2008'])
+  $years.unshift(['2012', '2012'])
+
   def index
   end
 
@@ -12,6 +27,9 @@ class WelcomeController < ApplicationController
   end
 
   def search
+    @months = $months
+    @days = $days
+    @years = $years
   end
 
   def poster
